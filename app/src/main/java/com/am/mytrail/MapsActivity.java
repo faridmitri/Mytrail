def02ec38ad24f087df975d311adb2292c74aa3a
Farid Mitri <faridmitri@gmail.com>
2021-02-05 15:20:21 +0200

    map activity

--- app/src/main/java/com/am/mytrail/MapsActivity.java
+++ app/src/main/java/com/am/mytrail/MapsActivity.java
@@ -1,12 +1,17 @@
 package com.am.mytrail;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
@@ -14,6 +19,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -26,22 +32,25 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("SpellCheckingInspection")
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     LocationManager locationManager;
+
     LocationListener locationListener;
     double longitude;
     double lagitude;
     Boolean flag = false;
     LatLng loc,loc1;
-
     Marker mCurrLocationMarker;
+    ConstraintLayout constraintLayout;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -49,10 +58,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startListening();
 
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-            }
+        }
+    }
+
+    public void startListening() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
         }
     }
 
@@ -60,8 +76,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-
+        constraintLayout = findViewById(R.id.constraintlayout);
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -80,22 +95,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
-
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+  /*      if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Toast.makeText(this, "GPS is Enabled in your devide", Toast.LENGTH_SHORT).show();
+        }else{
+            showGPSDisabledAlertToUser();
+        }*/
+        userlocation();
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         // Add a marker in Sydney and move the camera
-        userlocation();
 
 
     }
 
 
 public void userlocation() {
-    LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
     LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(@NonNull Location location) {
+
             longitude = location.getLongitude();
             lagitude = location.getLatitude();
             loc = new LatLng(lagitude, longitude);
@@ -104,6 +124,7 @@ public void userlocation() {
             {
                 loc1=loc;
                 flag=true;
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 15));
             }
 
             if (mCurrLocationMarker == null ) {
@@ -114,7 +135,10 @@ public void userlocation() {
             }
 
 
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 15));
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+
+
             if(!loc.equals(loc1)) {
                 mMap.addPolyline((new PolylineOptions())
                         .add(loc1, loc).width(6).color(Color.BLUE)
@@ -126,6 +150,30 @@ public void userlocation() {
             }
 
         }
+        public void onProviderDisabled(String s) {
+            if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                Toast.makeText(MapsActivity.this, "GPS is Enabled in your devide", Toast.LENGTH_SHORT).show();
+            }else {
+                Snackbar snackbar = Snackbar.make(constraintLayout, "GPS Desabled, Go to settings and enable it", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Settings", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+                snackbar.show();
+            }
+
+        }
+
+        public void onProviderEnabled(String s) {
+            // SettingSaved.LoadData(context);
+            Toast.makeText(MapsActivity.this,
+                    " GPS turned on. you can follow your locations",
+                    Toast.LENGTH_LONG).show();
+        }
     };
     if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
@@ -133,6 +181,30 @@ public void userlocation() {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 }
+
+
+    private void showGPSDisabledAlertToUser(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Goto Settings Page To Enable GPS",
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+
 
 
 }
